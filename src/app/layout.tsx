ba7f93f312f8f308ceeaac9eb229c9a5ceba94b1
Nereu Jr <nnvljr86@gmail.com>
2025-08-02 21:39:30 +0000

    at AppDevOverlay (https://6000-firebase-studio-1754168459985.cluster-ets

--- src/app/layout.tsx
+++
@@ -1,6 +0,0 @@
-// This file is no longer used for the main layout. 
-// The layout has been moved to src/app/[locale]/layout.tsx to support internationalization.
-// You can delete this file.
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-// This file is no longer used for the main page.
-// The page has been moved to src/app/[locale]/page.tsx to support internationalization.
-// You can delete this file.
-export default function Home() {
-  return null;
-}
