95ddfecda6dad4ae43815d8e14ecf2b9589876c5
Nereu Jr <nnvljr86@gmail.com>
2025-08-03 00:22:49 +0000

    O EXEMPLO DO CODIGO "o exemplo de codigo ta errado, é assim
    "Fuzil de

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -82,7 +82,7 @@ export const builds: Build[] = [
   {
     id: 'b5',
     name: 'Conquistador G3',
-    shareCode: 'G3-Conquista-6H3LATG081MQDPAGJAK1I',
+    shareCode: '6H3LATG081MQDPAGJAK1I',
     baseWeapon: 'G3',
     author: users[2],
     tags: ['Long Range', 'High Damage', 'Tactical'],
