264771d102a1b905a9ff6eeec8f072e02abd9d0d
Nereu Jr <nnvljr86@gmail.com>
2025-08-03 04:02:42 +0000

    esta todo extourado o layout

--- src/app/[locale]/admin/layout.tsx
+++ src/app/[locale]/admin/layout.tsx
@@ -70,45 +70,45 @@ export default function AdminLayout({
 
   return (
     <SidebarProvider>
-        <div className="flex min-h-screen w-full flex-col bg-muted/40">
-            <AdminSidebar />
-            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-                <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-                    <SidebarTrigger className="sm:hidden" />
-                    
-                    <div className="relative ml-auto flex-1 md:grow-0">
-                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                        <Input
-                        type="search"
-                        placeholder="Search..."
-                        className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
-                        />
-                    </div>
-                     <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="outline" size="icon" className="overflow-hidden rounded-full">
-                               <Avatar>
-                                    <AvatarImage src={currentUser.avatarUrl} alt={currentUser.name} />
-                                    <AvatarFallback>{currentUser.name.charAt(0)}</AvatarFallback>
-                                </Avatar>
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuLabel>{currentUser.name}</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                             <DropdownMenuItem asChild>
-                                <Link href={`/profile/${currentUser.name}`}><UserIcon className="mr-2 h-4 w-4" /> Profile</Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem><LogOut className="mr-2 h-4 w-4"/> Sign Out</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </header>
-                <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-                    {children}
-                </main>
-            </div>
+      <div className="flex min-h-screen w-full bg-muted/40">
+        <AdminSidebar />
+        <div className="flex flex-1 flex-col sm:gap-4 sm:py-4 sm:pl-14">
+            <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+                <SidebarTrigger className="sm:hidden" />
+                
+                <div className="relative ml-auto flex-1 md:grow-0">
+                    <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                    <Input
+                    type="search"
+                    placeholder="Search..."
+                    className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
+                    />
+                </div>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon" className="overflow-hidden rounded-full">
+                            <Avatar>
+                                <AvatarImage src={currentUser.avatarUrl} alt={currentUser.name} />
+                                <AvatarFallback>{currentUser.name.charAt(0)}</AvatarFallback>
+                            </Avatar>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>{currentUser.name}</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                          <DropdownMenuItem asChild>
+                            <Link href={`/profile/${currentUser.name}`}><UserIcon className="mr-2 h-4 w-4" /> Profile</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem><LogOut className="mr-2 h-4 w-4"/> Sign Out</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </header>
+            <main className="flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+                {children}
+            </main>
         </div>
+      </div>
     </SidebarProvider>
   );
 }
